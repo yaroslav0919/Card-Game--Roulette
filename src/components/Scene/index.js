5b5747786a74e80ae1ba28d44bc92bec6f0d8afe
yaroslav0919 <shamoyar1313@gmail.com>
2022-10-20 09:50:39 -0400

    add sparkle moving animation

--- src/components/Scene/index.js
+++ src/components/Scene/index.js
@@ -7,60 +7,122 @@ import useEvents from './useEvents'
 import useStore from '../../store'
 import useEntranceAnimation from './useEntranceAnimation'
 import useResource from './useResource'
+import useSparkleAnim from './useSparkleAnim'
+
+import tableData from '../../constants/table'
 
 export default function Scene () {
-  const ref = useRef(null)
+	const ref = useRef(null)
 
-  const { drawNormalTable } = useNormalTable()
-  const { drawRaceTrackTable } = useRaceTrackTable()
-  const { onPointerDownHandler } = useEvents()
-  const { addEntranceAnimation } = useEntranceAnimation()
-  const { preLoadSpriteImages } = useResource()
+	const { drawNormalTable } = useNormalTable()
+	const { drawRaceTrackTable } = useRaceTrackTable()
+	const { onPointerDownHandler } = useEvents()
+	const { addEntranceAnimation } = useEntranceAnimation()
+	const { preLoadSpriteImages } = useResource()
 
-  const [heatMapMode, setHeatMapMode] = useState(false)
+	const { addSparkleAnimation } = useSparkleAnim()
 
-  const setApp = useStore((state) => state.setApp)
+	const [heatMapMode, setHeatMapMode] = useState(false)
 
-  useEffect(() => {
-    const app = new Application({
-      resizeTo: window,
-      backgroundAlpha: 0,
-      width: window.innerWidth,
-      height: window.innerHeight,
-      antialias: true
-    })
+	const setApp = useStore((state) => state.setApp)
 
-    setApp(app)
+  const playSparkleAnimation = (app) => {
+		const calcCenterOffset = () => {
+			const btn0 = tableData.find((item) => item.key === 'bn-0')
+			const btnEnd = tableData.find((item) => item.key === 'bg-3-36')
+		
+			const startPos = {
+				x: btn0?.bordersPos[0].x - 30,
+				y: btn0?.bordersPos[0].y + 50
+			}
+		
+			const endPos = {
+				x: (btnEnd?.bordersPos[0].x + btnEnd?.bordersPos[0].w),
+				y: (btnEnd?.bordersPos[0].y + btnEnd?.bordersPos[0].h)
+			}
+		
+			const centerPoint = {
+				x: startPos.x + (endPos.x - startPos.x) / 2,
+				y: startPos.y + (endPos.y - startPos.y) / 2
+			}
+		
+			const screenCenter = {
+				x: window.innerWidth / 2,
+				y: window.innerHeight / 2
+			}
+		
+			const offset = {
+				x: screenCenter.x - centerPoint.x,
+				y: screenCenter.y - centerPoint.y
+			}
+		
+			return offset
+		}
 
-    ref.current.appendChild(app.view)
+		const getNumberPosition = (number) => {
+			const btn = tableData.find((item) => {
+				return item.key === `bn-${number}`
+			})
 
-    app.view.addEventListener('pointerdown', onPointerDownHandler)
+			const centerOffset = calcCenterOffset()
+			
+			const x = btn.bordersPos[0].x + btn.bordersPos[0].w / 2 + centerOffset.x
+			const y = btn.bordersPos[0].y + btn.bordersPos[0].h / 2 + centerOffset.y
 
-    app.start()
+			return { x, y }
+		}
 
-    // drawRaceTrackTable(app)
-    drawNormalTable(app, heatMapMode)
+		const points = []
+		points.push(getNumberPosition(24))
+		points.push(getNumberPosition(16))
+		points.push(getNumberPosition(5))
 
-    const loadAndPlayAnimation = async () => {
-      const loader = preLoadSpriteImages()
-      loader.onComplete.add(() => {
-        addEntranceAnimation(app)
-      })
-      // console.error('hehehe')
-    }
+    	addSparkleAnimation(app, points)
+  }
 
-    loadAndPlayAnimation()
+	useEffect(() => {
+		const app = new Application({
+			resizeTo: window,
+			backgroundAlpha: 0,
+			width: window.innerWidth,
+			height: window.innerHeight,
+			antialias: true
+		})
 
-    return () => {
-      app.view.removeEventListener('pointerdown', onPointerDownHandler)
-      app.destroy(true, true)
-    }
-  }, [])
+		setApp(app)
 
-  return (
-    <>
-      {heatMapMode ? <HeatMapView /> : null}
-      <div className='canvasScene' ref={ref} />
-    </>
-  )
+		ref.current.appendChild(app.view)
+
+		app.view.addEventListener('pointerdown', onPointerDownHandler)
+
+		app.start()
+
+		// drawRaceTrackTable(app)
+
+		const loadAndPlayAnimation = () => {
+			const loader = preLoadSpriteImages()
+			loader.onComplete.add(() => {
+				drawNormalTable(app, heatMapMode)
+				addEntranceAnimation(app)
+
+				setTimeout(() => {
+					playSparkleAnimation(app)
+				}, 3000)
+			})
+		}
+
+		loadAndPlayAnimation()
+
+		return () => {
+			app.view.removeEventListener('pointerdown', onPointerDownHandler)
+			app.destroy(true, true)
+		}
+	}, [])
+
+	return (
+		<>
+			{heatMapMode ? <HeatMapView /> : null}
+			<div className='canvasScene' ref={ref} />
+		</>
+	)
 }
---
+++ src/components/Scene/useSparkleAnim.js
@@ -0,0 +1,177 @@
+import * as PIXI from 'pixi.js'
+import * as Particles from '@pixi/particle-emitter'
+
+import { gsap } from 'gsap'
+import { PixiPlugin } from 'gsap/PixiPlugin'
+
+PixiPlugin.registerPIXI(PIXI)
+
+export default function useSparkleAnim() {
+    const addSparkleAnimation = (app, points) => {
+        const container = new PIXI.Container()
+        app.stage.addChild(container)
+
+        const initPos = {
+            x: window.innerWidth * 38 / 100,
+            y: window.innerHeight - window.innerWidth * 30 / 100,
+        }
+
+        const emitter = new Particles.Emitter(
+            container,
+            {
+                "lifetime": {
+                    "min": 0.2,
+                    "max": 0.8
+                },
+                "frequency": 0.0005,
+                "emitterLifetime": -1,
+                "maxParticles": 9999,
+                "addAtBack": false,
+                "pos": {
+                    "x": initPos.x,
+                    "y": initPos.y
+                },
+                "behaviors": [
+                    {
+                        "type": "alpha",
+                        "config": {
+                            "alpha": {
+                                "list": [
+                                    {
+                                        "time": 0,
+                                        "value": 0.8
+                                    },
+                                    {
+                                        "time": 1,
+                                        "value": 0.1
+                                    }
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        "type": "moveSpeed",
+                        "config": {
+                            "speed": {
+                                "list": [
+                                    {
+                                        "time": 0,
+                                        "value": 30
+                                    },
+                                    {
+                                        "time": 1,
+                                        "value": 15
+                                    }
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        "type": "scale",
+                        "config": {
+                            "scale": {
+                                "list": [
+                                    {
+                                        "time": 0,
+                                        "value": 0.2
+                                    },
+                                    {
+                                        "time": 1,
+                                        "value": 0.1
+                                    }
+                                ]
+                            },
+                            "minMult": 0.5
+                        }
+                    },
+                    {
+                        "type": "color",
+                        "config": {
+                            "color": {
+                                "list": [
+                                    {
+                                        "time": 0,
+                                        "value": "ffcf5b"
+                                    },
+                                    {
+                                        "time": 1,
+                                        "value": "ffffff"
+                                    }
+                                ]
+                            }
+                        }
+                    },
+                    {
+                        "type": "rotationStatic",
+                        "config": {
+                            "min": 0,
+                            "max": 360
+                        }
+                    },
+                    {
+                        "type": "textureRandom",
+                        "config": {
+                            "textures": [
+                                "/assets/images/particle.png"
+                            ]
+                        }
+                    },
+                    {
+                        "type": "spawnShape",
+                        "config": {
+                            "type": "torus",
+                            "data": {
+                                "x": 0,
+                                "y": 0,
+                                "radius": 1,
+                                "innerRadius": 0,
+                                "affectRotation": false
+                            }
+                        }
+                    }
+                ]
+            }
+        )
+
+        var elapsed = Date.now()
+
+        let pointIndex = 0
+        const frameCount = 60
+        const speed = {
+            x: 0,
+            y: 0
+        }
+
+        points = [ initPos, ...points ]
+
+        let canStart = false
+        setTimeout(() => {
+            canStart = true
+        }, 1000)
+        
+        app.ticker.add(() => {
+            if( canStart && Math.ceil(emitter.spawnPos.x) === Math.ceil(points[pointIndex].x) && Math.ceil(emitter.spawnPos.y) === Math.ceil(points[pointIndex].y) ) {
+                pointIndex ++
+                if( pointIndex >= points.length ) {
+                    speed.x = 0
+                    speed.y = 0
+                    pointIndex = points.length - 1
+                } else {
+                    speed.x = ( points[pointIndex].x - emitter.spawnPos.x ) / frameCount
+                    speed.y = ( points[pointIndex].y - emitter.spawnPos.y ) / frameCount
+                }
+            }
+
+            emitter.spawnPos.x += speed.x
+            emitter.spawnPos.y += speed.y
+
+            const now = Date.now()
+            emitter.update((now - elapsed) * 0.001)
+            elapsed = now
+        })
+
+        emitter.emit = true
+    }
+
+    return { addSparkleAnimation }
+}
