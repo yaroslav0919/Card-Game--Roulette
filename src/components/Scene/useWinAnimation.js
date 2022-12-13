a552429b7e7d1907a46acbb938f2b33d06422949
yaroslav0919 <shamoyar1313@gmail.com>
2022-12-13 02:33:58 +0000

    update fire

--- src/components/Scene/useWinAnimation.js
+++ src/components/Scene/useWinAnimation.js
@@ -1,7 +1,9 @@
 import * as PIXI from "pixi.js";
 import { gsap } from "gsap";
+import MotionPathPlugin from "gsap/MotionPathPlugin";
+import { Emitter } from "pixi-particles";
 import { ang2Rad } from "../../utils/math";
-import { fire } from "./useFireworks";
+import { fire, loop, stop } from "./useFireworks";
 export default function useWinAnimation() {
   const addDefaultAnim = (app, winType, timeLine) => {
     const container = new PIXI.Container();
@@ -103,8 +105,9 @@ export default function useWinAnimation() {
     g.beginFill(0xffffff);
     g.drawRect(0, 0, 50, 20);
     g.endFill();
-    for (let i = 1; i < 10; i++) {
-      g.beginFill(0xffffff, 1 - i / 10);
+    const blurEffect = 30;
+    for (let i = 1; i < blurEffect; i++) {
+      g.beginFill(0xffffff, 1 - i / blurEffect);
       g.drawRect(49, 0, 1, 20);
       g.endFill();
     }
@@ -116,14 +119,14 @@ export default function useWinAnimation() {
     mask.width = (winText.width * 6) / 5;
     mask.height = winText.height;
     mask.anchor.set(0);
-    // container.addChild(mask);
-    // winText.mask = mask;
-    // winText2.mask = mask;
+    container.addChild(mask);
+    winText.mask = mask;
+    winText2.mask = mask;
     timeLine.add(
       gsap.from(mask, {
         width: 0,
         duration: 1.5,
-        delay: 0.5,
+        delay: 1,
         ease: "none",
       }),
       "<"
@@ -158,6 +161,7 @@ export default function useWinAnimation() {
         onComplete: () => {
           gsap.to(earnText, {
             duration: 0.5,
+            delay: 0.5,
             width: earnText.width * 1.2,
             height: earnText.height * 1.2,
             repeat: 1,
@@ -170,19 +174,146 @@ export default function useWinAnimation() {
 
     container.addChild(earnText);
   };
-  const addFireworksAnim = (t2) => {
-    t2.add(fire(216, 310, 216, 200));
+  const addFireworksAnim = async (t2) => {
+    loop();
+    fire(216, 310, 216, 200);
+    gsap.delayedCall(5, () => fire(216, 310, 350, 200));
+    gsap.delayedCall(5.5, () => fire(216, 310, 100, 200));
+    gsap.delayedCall(6, () => fire(216, 310, 350, 200));
+    gsap.delayedCall(8, () => stop());
   };
-  const addFireframeAnim = (t2) => {};
+  const addSign = (app, t2) => {
+    const texture = new PIXI.Texture.from("/assets/image/spot.png");
+    const startPos = { x: 100, y: 250 };
+    const emitter = new Emitter(app.stage, texture, {
+      alpha: {
+        start: 1,
+        end: 0.66,
+      },
+      scale: {
+        start: 0.1,
+        end: 0.01,
+        minimumScaleMultiplier: 3,
+      },
+      color: {
+        start: "#fff6c9",
+        end: "#ffb119",
+      },
+      speed: {
+        start: 50,
+        end: 10,
+        minimumSpeedMultiplier: 0.5,
+      },
+      acceleration: {
+        x: 0,
+        y: 0,
+      },
+      maxSpeed: 0,
+      startRotation: {
+        min: 0,
+        max: 360,
+      },
+      noRotation: false,
+      rotationSpeed: {
+        min: 5,
+        max: 5,
+      },
+      lifetime: {
+        min: 0.3,
+        max: 0.9,
+      },
+      blendMode: "screen",
+      frequency: 0.001,
+      emitterLifetime: -1,
+      maxParticles: 500,
+      pos: {
+        x: startPos.x,
+        y: startPos.y,
+      },
+      addAtBack: false,
+      spawnType: "circle",
+      spawnCircle: {
+        x: 0,
+        y: 0,
+        r: 0,
+      },
+    });
+
+    gsap.registerPlugin(MotionPathPlugin);
+    emitter.emit = true;
+    gsap.to(emitter.spawnPos, {
+      motionPath: [
+        { x: startPos.x, y: startPos.y },
+        { x: startPos.x + 50, y: startPos.y - 10 },
+        { x: startPos.x + 60, y: startPos.y + 10 },
+        { x: startPos.x + 200, y: startPos.y - 20 },
+      ],
+      duration: 1,
+      delay: 1,
+      onComplete: () => {
+        emitter.emit = false;
+      },
+    });
+
+    let elapsed = Date.now();
+    const update = function () {
+      requestAnimationFrame(update);
+      const now = Date.now();
+      emitter.update((now - elapsed) * 0.001);
+      elapsed = now;
+    };
+    emitter.emit = true;
+    update();
+  };
+  const addFireframeAnim = (app) => {
+    const oneFire = (num, count, upon) => {
+      const path = `/assets/frames/fire${num}/f`;
+      const frames = [];
+
+      for (let i = 1; i <= count; i++) {
+        const texture = PIXI.Texture.from(path + "(" + i + ")" + ".png");
+        frames.push(texture);
+      }
+      return frames;
+      // const sprite = new PIXI.AnimatedSprite(frames);
+      // sprite.scale.set(1 / 5, 1 / 5);
+      // sprite.anchor.set(0.5);
+      // sprite.x = window.innerWidth / 2;
+      // sprite.y = count > 2 ? 130 : 230;
+
+      // sprite.zIndex = upon ? 0 : -1;
+      // sprite.loop = num > 2 ? true : false;
+      // sprite.animationSpeed = 0.1;
+
+      // sprite.play();
+      // app.stage.addChild(sprite);
+    };
+    const first = [...oneFire(1, 15, true), ...oneFire(2, 20, true)];
+    // const timeline = new gsap.timeline({ ease: "none", paused: true });
+    // timeline.add(() => oneFire(1, 15, true), "<3");
+    // timeline.add(() => oneFire(2, 20, true), "<");
+    // timeline.add(() => oneFire(3, 9, false), "<");
+    // timeline.add(() => oneFire(4, 9, false), "<");
+    // timeline.add(() => oneFire(5, 9, false), "<");
+    // timeline.add(() => oneFire(6, 9, false), "<");
+
+    // timeline.play();
+    // oneFire(3, 9, false);
+    // oneFire(4, 9, false);
+    // oneFire(5, 9, false);
+    // oneFire(6, 9, false);
+    // oneFire(1, 15, true);
+  };
   const winAnim = async (app, winType) => {
     const t2 = new gsap.timeline({ ease: "none", paused: true });
     addDefaultAnim(app, winType, t2);
     switch (winType) {
       case "BIG WIN":
-        addFireworksAnim(t2);
+        addFireworksAnim();
+        addSign(app, t2);
         break;
-      case "SENSATIONAL WIN":
-        addFireframeAnim(t2);
+      case "SENSATIONAL":
+        addFireframeAnim(app);
         break;
     }
     t2.play();
